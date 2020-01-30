b50e487f2b5d749b4a372a9dd6dd06f54143ef91
Mohammed Maher <maher@uomosul.edu.iq>
2020-01-30 13:50:41 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, {[key]: value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const cloneDriver =  Object.assign({}, driver);
+  delete cloneDriver[key];
+  return cloneDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
