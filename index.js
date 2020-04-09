60275a3cdf98ec47be35cdf2595ce4843d3aaacb
Allan Saleh <allan@re-coded.com>
2020-04-09 09:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromDriverByKey(driver, key){
     return newDriver;
 }
 
-function deleteFromDriverByKey(driver, key){
+function destructivelyDeleteFromDriverByKey(driver, key){
     delete driver[key];
     return driver;
 }
